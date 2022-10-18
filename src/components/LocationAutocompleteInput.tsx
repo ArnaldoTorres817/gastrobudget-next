bf1b9decc78da0d49ed8c98f909b2ec2d509d608
Arnaldo Torres <torres.arnaldo817@gmail.com>
2022-10-18 17:16:34 -0400

    wip: form

--- src/components/LocationAutocompleteInput.tsx
+++ src/components/LocationAutocompleteInput.tsx
@@ -1,7 +1,46 @@
-import { FC } from 'react'
+import {
+  CloseSmall as CloseIcon,
+  LocalTwo as LocationIcon,
+} from '@icon-park/react'
+
+import type { FC } from 'react'
+import type { Location } from '@/types/location'
 
 const LocationAutocompleteInput: FC = () => {
-  return <></>
+  function getFormattedLocation(location: Location): string {
+    if (location.city !== undefined) {
+      return `${location.city}, ${location.state}`
+    }
+
+    return `${location.state}, ${location.country}`
+  }
+
+  return (
+    <div className="w-80 relative flex flex-col items-center">
+      <div className="relative w-full">
+        <input
+          type="text"
+          required
+          className="w-full pl-4 pr-6 py-3 border rounded-xl focus-visible:outline-primary focus-visible:outline focus-visible:outline-2"
+          id="location"
+          name="location"
+        />
+        <button
+          type="button"
+          className="absolute top-1/2 -translate-y-1/2 right-2"
+        >
+          <CloseIcon size={16} />
+        </button>
+      </div>
+
+      <ul className="absolute top-full z-10 w-full max-h-52 py-3 mt-1 border rounded shadow bg-white overflow-auto">
+        <li className="px-4 py-2 flex items-center gap-2 hover:bg-zinc-200 cursor-pointer">
+          <LocationIcon size={20} />
+          {/* {getFormattedLocation(location)} */}
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default LocationAutocompleteInput
---
+++ src/types/location.d.ts
@@ -0,0 +1,41 @@
+export interface Location {
+  datasource: Datasource
+  city: string
+  county: string
+  state: string
+  postcode: string
+  country: string
+  country_code: string
+  lon: number
+  lat: number
+  state_code: string
+  formatted: string
+  address_line1: string
+  address_line2: string
+  category: string
+  result_type: string
+  rank: Rank
+  place_id: string
+  bbox: Bbox
+}
+
+interface Datasource {
+  sourcename: string
+  attribution: string
+  license: string
+  url: string
+}
+
+interface Rank {
+  importance: number
+  confidence: number
+  confidence_city_level: number
+  match_type: string
+}
+
+interface Bbox {
+  lon1: number
+  lat1: number
+  lon2: number
+  lat2: number
+}
