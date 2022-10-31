a0635f7ebbdcd8bd88da5c70994bd75990bbdd86
Arnaldo Torres <torres.arnaldo817@gmail.com>
2022-10-30 21:41:05 -0400

    feat(api): :sparkles: add rewrites for third party apis

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/businesses/:path*',
+        destination: 'https://api.yelp.com/v3/businesses/:path*',
+      },
+      {
+        source: '/api/geocode/:path*',
+        destination: 'https://api.geoapify.com/v1/geocode/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
